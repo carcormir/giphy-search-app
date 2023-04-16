442d95bc5cfaae6de89d00e6634550288a1e6bf8
carcormir <carcormir@gmail.com>
2023-04-16 19:27:07 +0200

    handling edge cases for service request

--- src/services/getGifByIdService.jsx
+++ src/services/getGifByIdService.jsx
@@ -5,12 +5,18 @@ export default function getGifById ({
 }) {
   const SEARCH_URL = `${API_URL}/gifs/${id}?api_key=${API_KEY}`
 
+  if (!id) {
+    return Promise.reject(new Error('No ID provided'))
+  }
   return fetch(SEARCH_URL)
     .then(res => res.json())
     .then(resJson => {
       const { data } = resJson
       const { images, title, id } = data
-      const { url } = images.downsized_medium
+      const { url } = images.downsized_medium?.url || ''
       return { title, id, url }
+    })
+    .catch(err => {
+      return Promise.reject(new Error(`Failed to get GIF by ID ${id}: ${err.message}`))
     })
 }
