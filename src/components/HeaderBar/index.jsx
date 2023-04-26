b206aa13082a427894173c68e44a38ae6d2cef1a
carcormir <carcormir@gmail.com>
2023-04-26 15:21:32 +0200

    adjusting responsiveness for mobile and desktop

--- src/components/HeaderBar/index.jsx
+++ src/components/HeaderBar/index.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import './styles.css'
 import { useEffect, useState } from 'react'
 
+const MAX_WIDTH = 880
+const PAGE_OFFSET_MOBILE = 0
+const PAGE_OFFSET_DESKTOP = 40
 export default function HeaderBar () {
   const [scrolled, setScrolled] = useState(false)
 
@@ -14,7 +17,8 @@ export default function HeaderBar () {
   }, [])
 
   function handleScroll () {
-    if (window.pageYOffset > 40) {
+    if ((window.pageYOffset > PAGE_OFFSET_MOBILE && window.innerWidth <= MAX_WIDTH) ||
+        (window.pageYOffset > PAGE_OFFSET_DESKTOP && window.innerWidth > MAX_WIDTH)) {
       setScrolled(true)
     } else {
       setScrolled(false)
@@ -25,7 +29,7 @@ export default function HeaderBar () {
     <>
       <div className={`search-bar-wrapper ${scrolled ? 'sticky' : ''}`}>
         <Link to='/'>
-          <h1 className={`main-title scroll-top ${scrolled ? 'rotate' : ''}`}>Giphy Search App</h1>
+          <h1 className={`main-title scroll-top ${scrolled ? 'scaleme delay-1' : ''}`}>Giphy Search App</h1>
         </Link>
         <SearchForm />
       </div>
